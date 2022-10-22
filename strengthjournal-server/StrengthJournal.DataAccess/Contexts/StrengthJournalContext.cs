31b01954d8e18d81e648f1a13cff18bbf0481c84
Joel Johnston <colter13@gmail.com>
2022-10-22 14:58:03 -0600

    Bootstrap Data Access Layer

---
+++ strengthjournal-server/StrengthJournal.DataAccess/Contexts/StrengthJournalContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StrengthJournal.DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthJournal.DataAccess.Contexts
+{
+    public class StrengthJournalContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=localhost;Database=StrengthJournal1;Trusted_Connection=True");
+        }
+    }
+}
---
+++ strengthjournal-server/StrengthJournal.DataAccess/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthJournal.DataAccess.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [MaxLength(255)]
+        public string Handle { get; set; }
+    }
+}
