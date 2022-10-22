ce74594677dcddc8db2c8db00d9ee2ef42b08eba
Joel Johnston <colter13@gmail.com>
2022-10-22 13:31:45 -0600

    Add route for Angular app

---
+++ strengthjournal-server/StrengthJournal.Server/Controllers/AppController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StrengthJournal.Server.Controllers
+{
+    public class AppController : Controller
+    {
+        [Route("app/{*more}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
