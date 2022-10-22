f6ea3873be41742a747af37f3e56dc44399bbeec
Joel Johnston <colter13@gmail.com>
2022-10-22 14:27:49 -0600

    Get angular application loading from MVC view

--- strengthjournal-server/StrengthJournal.Server/Controllers/AppController.cs
+++ strengthjournal-server/StrengthJournal.Server/Controllers/AppController.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StrengthJournal.Server.Models;
 
 namespace StrengthJournal.Server.Controllers
 {
     public class AppController : Controller
     {
+        private IWebHostEnvironment _hostEnvironment;
+
+        public AppController(IWebHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
         [Route("app/{*more}")]
         public IActionResult Index()
         {
-            return View();
+            return View(GetAppViewModel());
+        }
+
+        protected AppViewModel GetAppViewModel()
+        {
+            var distFiles = Directory.GetFiles(Path.Combine(this._hostEnvironment.WebRootPath, "dist/")).Select(path => Path.GetFileName(path)).ToList();
+            var runtimeFileName = distFiles.FirstOrDefault(f => f.StartsWith("runtime")) ?? throw new Exception("No runtime file in Angular build output");
+            var polyfillsFileName = distFiles.FirstOrDefault(f => f.StartsWith("polyfills")) ?? throw new Exception("No polyfills file in Angular build output");
+            var mainFileName = distFiles.FirstOrDefault(f => f.StartsWith("main")) ?? throw new Exception("No main file in Angular build output");
+            return new AppViewModel(
+                ExtractHashFromAngularDistFile(runtimeFileName),
+                ExtractHashFromAngularDistFile(polyfillsFileName),
+                ExtractHashFromAngularDistFile(mainFileName)
+            );
+        }
+
+        protected string ExtractHashFromAngularDistFile(string fileName)
+        {
+            return fileName.Split('.').ElementAt(1);
         }
     }
 }
---
+++ strengthjournal-server/StrengthJournal.Server/Models/AppViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace StrengthJournal.Server.Models
+{
+    public class AppViewModel
+    {
+        protected const string appDistBase = "dist";
+
+        protected readonly string runtimeHash;
+        protected readonly string polyfillsHash;
+        protected readonly string mainHash;
+
+        public string runtimeUrl { get => $"{appDistBase}/runtime.{runtimeHash}.js"; }
+        public string polyfillsUrl { get => $"{appDistBase}/polyfills.{polyfillsHash}.js"; }
+        public string mainUrl { get => $"{appDistBase}/main.{mainHash}.js"; }
+
+        public AppViewModel(string runtimehash, string polyfillsHash, string mainHash)
+        {
+            this.runtimeHash = runtimehash;
+            this.polyfillsHash = polyfillsHash;
+            this.mainHash = mainHash;
+        }
+    }
+}
