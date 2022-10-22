ca400ad80cfa04e22b768a501bda2298bfcc8fbe
Joel Johnston <colter13@gmail.com>
2022-10-22 14:39:17 -0600

    API controller proof of concept

---
+++ strengthjournal-server/StrengthJournal.Server/Controllers/API/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StrengthJournal.Server.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [Route("ping")]
+        public ActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
