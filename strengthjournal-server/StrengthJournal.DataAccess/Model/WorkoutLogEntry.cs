933a86b0534dc09ef73d7f28976fed7f2fe4a7df
Joel Johnston <colter13@gmail.com>
2022-10-30 18:01:15 -0600

    Extend the workout entity with title and notes

--- strengthjournal-server/StrengthJournal.DataAccess/Model/WorkoutLogEntry.cs
+++ strengthjournal-server/StrengthJournal.DataAccess/Model/WorkoutLogEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,13 @@ namespace StrengthJournal.DataAccess.Model
     public class WorkoutLogEntry
     {
         public Guid Id { get; set; }
+        [MaxLength(255)]
+        public string Title { get; set; } = "";
         public DateTime EntryDateUTC { get; set; }
         public User User { get; set; }
         public ICollection<WorkoutLogEntrySet> Sets { get; set; } = new List<WorkoutLogEntrySet>();
+        public uint? BodyWeightPIT { get; set; }
+        public string Notes { get; set; } = string.Empty;
+
     }
 }
