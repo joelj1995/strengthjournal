dbe2a28e2256b7affc6917242db25f3554349e58
Joel Johnston <colter13@gmail.com>
2022-11-05 15:33:33 -0600

    Add handler for unexpected errors

---
+++ strengthjournal-server/StrengthJournal.Server/Models/ExceptionHandlerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StrengthJournal.Server.Models
+{
+    public class ExceptionHandlerViewModel
+    {
+        public Guid? ErrorId { get; set; }
+        public bool ShowTrace { get; set; }
+    }
+}
