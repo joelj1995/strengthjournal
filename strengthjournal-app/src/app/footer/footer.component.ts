08eb8292bdda011fc6d5f5674a5b347d330923b0
Joel Johnston <colter13@gmail.com>
2022-11-16 20:55:10 -0700

    Fix a couple visual issues

--- strengthjournal-app/src/app/footer/footer.component.ts
+++ strengthjournal-app/src/app/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class FooterComponent implements OnInit {
 
-  version: string = environment.version;
+  version: string = environment.version.slice(0, 6);
 
   constructor() { }
 
