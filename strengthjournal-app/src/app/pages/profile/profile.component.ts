6555dc5de729f116f519c69d1ca42f8ba8f9d196
Joel Johnston <colter13@gmail.com>
2022-11-18 12:45:50 -0700

    Work on profile editor

--- strengthjournal-app/src/app/pages/profile/profile.component.ts
+++ strengthjournal-app/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ProfileSettings } from 'src/app/model/profile-settings';
 import { ProfileService } from 'src/app/services/profile.service';
 
@@ -11,11 +12,20 @@ export class ProfileComponent implements OnInit {
 
   settings: ProfileSettings | null = null;
 
+  settingsForm = new FormGroup({
+    preferredWeightUnit: new FormControl(''),
+    consentCEM: new FormControl('')
+  });
+
   constructor(private profile: ProfileService) { }
 
   ngOnInit(): void {
     this.profile.getSettings().subscribe(settings => {
       this.settings = settings;
+      this.settingsForm.setValue({
+        'preferredWeightUnit': settings.preferredWeightUnit,
+        'consentCEM': settings.consentCEM
+      });
     });
   }
 
