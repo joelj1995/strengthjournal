dd8308d1b76771ab2deabef443a96fab1dc071a0
Joel Johnston <colter13@gmail.com>
2022-11-18 18:58:51 -0700

    Seed the countries table

---
+++ strengthjournal-schema/seeding-scripts/countries.sql
@@ -0,0 +1,498 @@
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AD', N'Andorra')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AE', N'United Arab Emirates')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AF', N'Afghanistan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AG', N'Antigua and Barbuda')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AI', N'Anguilla')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AL', N'Albania')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AM', N'Armenia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AO', N'Angola')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AQ', N'Antarctica')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AR', N'Argentina')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AS', N'American Samoa')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AT', N'Austria')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AU', N'Australia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AW', N'Aruba')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AX', N'?àland Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'AZ', N'Azerbaijan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BA', N'Bosnia and Herzegovina')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BB', N'Barbados')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BD', N'Bangladesh')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BE', N'Belgium')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BF', N'Burkina Faso')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BG', N'Bulgaria')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BH', N'Bahrain')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BI', N'Burundi')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BJ', N'Benin')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BL', N'Saint Barth??lemy')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BM', N'Bermuda')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BN', N'Brunei Darussalam')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BO', N'Bolivia, Plurinational State of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BQ', N'Bonaire, Sint Eustatius and Saba')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BR', N'Brazil')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BS', N'Bahamas')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BT', N'Bhutan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BV', N'Bouvet Island')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BW', N'Botswana')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BY', N'Belarus')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'BZ', N'Belize')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CA', N'Canada')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CC', N'Cocos (Keeling) Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CD', N'Congo, the Democratic Republic of the')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CF', N'Central African Republic')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CG', N'Congo')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CH', N'Switzerland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CI', N'C??te d''Ivoire')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CK', N'Cook Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CL', N'Chile')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CM', N'Cameroon')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CN', N'China')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CO', N'Colombia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CR', N'Costa Rica')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CU', N'Cuba')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CV', N'Cape Verde')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CW', N'Cura?ºao')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CX', N'Christmas Island')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CY', N'Cyprus')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'CZ', N'Czech Republic')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'DE', N'Germany')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'DJ', N'Djibouti')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'DK', N'Denmark')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'DM', N'Dominica')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'DO', N'Dominican Republic')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'DZ', N'Algeria')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'EC', N'Ecuador')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'EE', N'Estonia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'EG', N'Egypt')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'EH', N'Western Sahara')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ER', N'Eritrea')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ES', N'Spain')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ET', N'Ethiopia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'FI', N'Finland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'FJ', N'Fiji')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'FK', N'Falkland Islands (Malvinas)')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'FM', N'Micronesia, Federated States of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'FO', N'Faroe Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'FR', N'France')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GA', N'Gabon')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GB', N'United Kingdom')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GD', N'Grenada')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GE', N'Georgia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GF', N'French Guiana')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GG', N'Guernsey')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GH', N'Ghana')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GI', N'Gibraltar')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GL', N'Greenland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GM', N'Gambia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GN', N'Guinea')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GP', N'Guadeloupe')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GQ', N'Equatorial Guinea')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GR', N'Greece')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GS', N'South Georgia and the South Sandwich Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GT', N'Guatemala')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GU', N'Guam')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GW', N'Guinea-Bissau')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'GY', N'Guyana')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'HK', N'Hong Kong')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'HM', N'Heard Island and McDonald Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'HN', N'Honduras')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'HR', N'Croatia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'HT', N'Haiti')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'HU', N'Hungary')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ID', N'Indonesia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IE', N'Ireland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IL', N'Israel')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IM', N'Isle of Man')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IN', N'India')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IO', N'British Indian Ocean Territory')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IQ', N'Iraq')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IR', N'Iran, Islamic Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IS', N'Iceland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'IT', N'Italy')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'JE', N'Jersey')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'JM', N'Jamaica')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'JO', N'Jordan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'JP', N'Japan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KE', N'Kenya')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KG', N'Kyrgyzstan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KH', N'Cambodia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KI', N'Kiribati')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KM', N'Comoros')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KN', N'Saint Kitts and Nevis')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KP', N'Korea, Democratic People''s Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KR', N'Korea, Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KW', N'Kuwait')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KY', N'Cayman Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'KZ', N'Kazakhstan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LA', N'Lao People''s Democratic Republic')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LB', N'Lebanon')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LC', N'Saint Lucia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LI', N'Liechtenstein')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LK', N'Sri Lanka')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LR', N'Liberia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LS', N'Lesotho')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LT', N'Lithuania')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LU', N'Luxembourg')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LV', N'Latvia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'LY', N'Libya')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MA', N'Morocco')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MC', N'Monaco')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MD', N'Moldova, Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ME', N'Montenegro')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MF', N'Saint Martin (French part)')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MG', N'Madagascar')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MH', N'Marshall Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MK', N'Macedonia, the Former Yugoslav Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ML', N'Mali')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MM', N'Myanmar')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MN', N'Mongolia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MO', N'Macao')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MP', N'Northern Mariana Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MQ', N'Martinique')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MR', N'Mauritania')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MS', N'Montserrat')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MT', N'Malta')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MU', N'Mauritius')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MV', N'Maldives')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MW', N'Malawi')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MX', N'Mexico')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MY', N'Malaysia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'MZ', N'Mozambique')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NA', N'Namibia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NC', N'New Caledonia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NE', N'Niger')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NF', N'Norfolk Island')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NG', N'Nigeria')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NI', N'Nicaragua')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NL', N'Netherlands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NO', N'Norway')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NP', N'Nepal')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NR', N'Nauru')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NU', N'Niue')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'NZ', N'New Zealand')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'OM', N'Oman')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PA', N'Panama')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PE', N'Peru')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PF', N'French Polynesia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PG', N'Papua New Guinea')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PH', N'Philippines')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PK', N'Pakistan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PL', N'Poland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PM', N'Saint Pierre and Miquelon')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PN', N'Pitcairn')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PR', N'Puerto Rico')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PS', N'Palestine, State of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PT', N'Portugal')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PW', N'Palau')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'PY', N'Paraguay')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'QA', N'Qatar')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'RE', N'R??union')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'RO', N'Romania')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'RS', N'Serbia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'RU', N'Russian Federation')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'RW', N'Rwanda')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SA', N'Saudi Arabia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SB', N'Solomon Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SC', N'Seychelles')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SD', N'Sudan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SE', N'Sweden')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SG', N'Singapore')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SH', N'Saint Helena, Ascension and Tristan da Cunha')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SI', N'Slovenia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SJ', N'Svalbard and Jan Mayen')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SK', N'Slovakia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SL', N'Sierra Leone')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SM', N'San Marino')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SN', N'Senegal')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SO', N'Somalia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SR', N'Suriname')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SS', N'South Sudan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ST', N'Sao Tome and Principe')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SV', N'El Salvador')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SX', N'Sint Maarten (Dutch part)')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SY', N'Syrian Arab Republic')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'SZ', N'Swaziland')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TC', N'Turks and Caicos Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TD', N'Chad')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TF', N'French Southern Territories')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TG', N'Togo')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TH', N'Thailand')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TJ', N'Tajikistan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TK', N'Tokelau')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TL', N'Timor-Leste')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TM', N'Turkmenistan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TN', N'Tunisia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TO', N'Tonga')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TR', N'Turkey')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TT', N'Trinidad and Tobago')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TV', N'Tuvalu')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TW', N'Taiwan, Province of China')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'TZ', N'Tanzania, United Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'UA', N'Ukraine')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'UG', N'Uganda')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'UM', N'United States Minor Outlying Islands')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'US', N'United States')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'UY', N'Uruguay')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'UZ', N'Uzbekistan')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VA', N'Holy See (Vatican City State)')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VC', N'Saint Vincent and the Grenadines')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VE', N'Venezuela, Bolivarian Republic of')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VG', N'Virgin Islands, British')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VI', N'Virgin Islands, U.S.')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VN', N'Viet Nam')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'VU', N'Vanuatu')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'WF', N'Wallis and Futuna')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'WS', N'Samoa')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'YE', N'Yemen')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'YT', N'Mayotte')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ZA', N'South Africa')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ZM', N'Zambia')
+GO
+INSERT [dbo].[Countries] ([Code], [Name]) VALUES (N'ZW', N'Zimbabwe')
+GO
