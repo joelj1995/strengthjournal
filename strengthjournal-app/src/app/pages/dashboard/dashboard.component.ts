4f700bb5487b2ba19a9358e6e1a8f59083cec50b
Joel Johnston <colter13@gmail.com>
2022-11-19 11:29:12 -0700

    Create bar chart for weekly volume report

--- strengthjournal-app/src/app/pages/dashboard/dashboard.component.ts
+++ strengthjournal-app/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeeklyVolumeReportLine } from 'src/app/model/weekly-volume-report-line';
 import { DashboardService } from 'src/app/services/dashboard.service';
+import { Chart, ChartConfiguration, ChartItem, registerables } from 'node_modules/chart.js'
 
 @Component({
   selector: 'app-dashboard',
@@ -16,7 +17,36 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.dashboard.getWeeklyVolumeReport().subscribe(lines => {
       this.weeklyVolumeLines = lines;
+      this.createWeeklyVolumeReportChart(lines)
     });
+  }
+
+  createWeeklyVolumeReportChart(lines: WeeklyVolumeReportLine[]) {
+    Chart.register(...registerables);
+    const data = {
+      labels: lines.map(l => new Date(l.weekStart).toLocaleDateString("en-US")),
+      datasets: [{
+        label: 'Nubmer of sets',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: lines.map(l => l.numberOfSets),
+      }]
+    };
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          display: false
+        }
+      }
+    };
+    const config: ChartConfiguration = {
+      type: 'bar',
+      data: data,
+      options: options
+    }
+    const chartItem: ChartItem = document.getElementById('my-chart') as ChartItem;
+    new Chart(chartItem, config)
   }
 
 }
