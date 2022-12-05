f54d400a273bc152639f53e0194002735b84bbb1
Joel Johnston <colter13@gmail.com>
2022-12-04 19:15:48 -0700

    Disable email update

--- strengthjournal-server/StrengthJournal.Server/Controllers/API/ProfileController.cs
+++ strengthjournal-server/StrengthJournal.Server/Controllers/API/ProfileController.cs
@@ -54,11 +54,12 @@ namespace StrengthJournal.Server.Controllers.API
         [HttpPut("email")]
         public async Task<ActionResult> UpdateEmail([FromBody] UpdateEmailDto updateEmail)
         {
-            var userId = HttpContext.GetUserId();
-            var result = await profileService.UpdateEmail(userId, updateEmail.NewEmail);
-            if (result)
-                return Ok();
-            return new StatusCodeResult(500);
+            throw new Exception("Endpoint disabled");
+            //var userId = HttpContext.GetUserId();
+            //var result = await profileService.UpdateEmail(userId, updateEmail.NewEmail);
+            //if (result)
+            //    return Ok();
+            //return new StatusCodeResult(500);
         }
 
         [HttpGet("config")]
