fe69f1c1eb372c9cafaa9475df10c4264e64c07d
Joel Johnston <colter13@gmail.com>
2022-12-10 14:25:14 -0700

    Add seeding scripts for Exercises and Weight Units

---
+++ strengthjournal-schema/seeding-scripts/exercises.sql
@@ -0,0 +1,6 @@
+INSERT [dbo].[Exercises] ([Id], [Name], [ParentExerciseId], [CreatedByUserId]) VALUES (N'bd99b901-26eb-4f12-90ca-91d6a7cc673d', N'Squat', NULL, NULL)
+GO
+INSERT [dbo].[Exercises] ([Id], [Name], [ParentExerciseId], [CreatedByUserId]) VALUES (N'f06fdf7f-76c0-4c67-8c3a-994009e75d5a', N'Bench Press', NULL, NULL)
+GO
+INSERT [dbo].[Exercises] ([Id], [Name], [ParentExerciseId], [CreatedByUserId]) VALUES (N'84e827a0-ca29-42f8-9b82-c0a79886fc30', N'Deadlift', NULL, NULL)
+GO
---
+++ strengthjournal-schema/seeding-scripts/weight-units.sql
@@ -0,0 +1,4 @@
+INSERT [dbo].[WeightUnits] ([Id], [FullName], [Abbreviation], [RatioToKg]) VALUES (N'4bc96550-f274-4a90-978b-92a398f8c49d', N'Kilograms', N'kg', CAST(1.00 AS Decimal(18, 2)))
+GO
+INSERT [dbo].[WeightUnits] ([Id], [FullName], [Abbreviation], [RatioToKg]) VALUES (N'bf8df35b-2f45-4a79-a49c-d3aca4a12cd6', N'Pounds', N'lbs', CAST(2.20 AS Decimal(18, 2)))
+GO
