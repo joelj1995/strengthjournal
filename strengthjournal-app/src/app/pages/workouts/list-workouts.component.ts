cf295f05640b8133953e995ff4d131c58ca01d72
Joel Johnston <colter13@gmail.com>
2022-12-20 12:00:58 -0700

    Use async operator for workout list

--- strengthjournal-app/src/app/pages/workouts/list-workouts.component.ts
+++ strengthjournal-app/src/app/pages/workouts/list-workouts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
 import { Workout } from 'src/app/model/workout';
 import { WorkoutService } from 'src/app/services/workout.service';
 
@@ -14,17 +15,17 @@ export class ListWorkoutsComponent implements OnInit {
   pageSize: number = 10;
   collectionSize: number = 0;
 
-  workoutList: Workout[] | null = null;
+  workoutList$: Observable<Workout[]> | undefined;
 
   stagedDelete: string | null = null;
 
   constructor(private workouts: WorkoutService, private router: Router) { }
 
   getWorkoutPage() {
-    this.workouts.getWorkouts(this.page, this.pageSize).subscribe(page => {
-      this.workoutList = page.data;
-      this.collectionSize = page.totalRecords;
-    });
+    this.workoutList$ = this.workouts.getWorkouts(this.page, this.pageSize).pipe(
+      tap(page => this.collectionSize = page.totalRecords),
+      map(page => page.data)
+    );
   }
 
   ngOnInit(): void {
@@ -42,7 +43,6 @@ export class ListWorkoutsComponent implements OnInit {
   confirmDeleteWorkout() {
     if (this.stagedDelete == null) throw "Tried to finalize workout delete but no ID staged";
     this.workouts.deleteWorkout(this.stagedDelete).subscribe(() => {
-      this.workoutList = this.workoutList?.filter(w => w.id != this.stagedDelete) ?? null;
       this.getWorkoutPage();
       this.stagedDelete = null;
     });
