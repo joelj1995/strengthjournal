274ceec50a080d1cc813df243f53253fc8215e4a
Joel Johnston <colter13@gmail.com>
2022-12-28 13:31:13 -0700

    Use virtual directory to serve SPA

--- strengthjournal-server/StrengthJournal.Server/Controllers/AppController.cs
+++ strengthjournal-server/StrengthJournal.Server/Controllers/AppController.cs
@@ -23,40 +23,5 @@ namespace StrengthJournal.Server.Controllers
             return View(model);
         }
 
-        [Route("app/assets/{*more}")]
-        public ActionResult Assets(string more)
-        {
-            return Redirect($"/dist/assets/{more}");
-        }
-
-        [Route("app/{*more}")]
-        public IActionResult Index(string more)
-        {
-            if (!String.IsNullOrEmpty(more) && more.EndsWith(".js"))
-            {
-                return Redirect($"/dist/{more}");
-            }
-            return View(GetAppViewModel());
-        }
-
-        protected AppViewModel GetAppViewModel()
-        {
-            var distFiles = Directory.GetFiles(Path.Combine(this._hostEnvironment.WebRootPath, "dist/")).Select(path => Path.GetFileName(path)).ToList();
-            var runtimeFileName = distFiles.FirstOrDefault(f => f.StartsWith("runtime")) ?? throw new Exception("No runtime file in Angular build output");
-            var polyfillsFileName = distFiles.FirstOrDefault(f => f.StartsWith("polyfills")) ?? throw new Exception("No polyfills file in Angular build output");
-            var mainFileName = distFiles.FirstOrDefault(f => f.StartsWith("main")) ?? throw new Exception("No main file in Angular build output");
-            var stylesFileName = distFiles.FirstOrDefault(f => f.StartsWith("styles")) ?? throw new Exception("No main file in Angular build output");
-            return new AppViewModel(
-                ExtractHashFromAngularDistFile(runtimeFileName),
-                ExtractHashFromAngularDistFile(polyfillsFileName),
-                ExtractHashFromAngularDistFile(mainFileName),
-                ExtractHashFromAngularDistFile(stylesFileName)
-            );
-        }
-
-        protected string ExtractHashFromAngularDistFile(string fileName)
-        {
-            return fileName.Split('.').ElementAt(1);
-        }
     }
 }
