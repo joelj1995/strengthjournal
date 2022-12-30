6d4acaac49d5a0c4d91650cfa2d228dcb0a63b50
Joel Johnston <colter13@gmail.com>
2022-12-30 09:52:40 -0700

    Change presentation component to use on push change detection

--- strengthjournal-app/src/app/workout/edit-workout/set-editor.component.ts
+++ strengthjournal-app/src/app/workout/edit-workout/set-editor.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Exercise } from 'src/app/model/exercise';
 
 @Component({
   selector: '[app-set-editor]',
   templateUrl: './set-editor.component.html',
-  styleUrls: ['./set-editor.component.css']
+  styleUrls: ['./set-editor.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SetEditorComponent implements OnInit {
 
