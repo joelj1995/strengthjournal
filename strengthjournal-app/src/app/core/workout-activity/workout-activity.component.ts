0ac7b5a05f00254b528997542ad8f44655a1d950
Joel Johnston <colter13@gmail.com>
2023-01-03 09:03:33 -0700

    Implement pagination for workout activity

--- strengthjournal-app/src/app/core/workout-activity/workout-activity.component.ts
+++ strengthjournal-app/src/app/core/workout-activity/workout-activity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, map, pipe, scan, switchMap, tap, withLatestFrom } from 'rxjs';
 import { WorkoutActivity } from 'src/app/model/workout-activity';
 import { WorkoutService } from 'src/app/services/workout.service';
 import { SubSink } from 'subsink';
@@ -10,6 +11,12 @@ import { SubSink } from 'subsink';
 })
 export class WorkoutActivityComponent implements OnInit, OnDestroy {
 
+  loadingMore: boolean = false;
+  noMoreRecords: boolean = false;
+
+  pageNumber$ = new BehaviorSubject(1);
+  readonly perPage = 5;
+
   workoutActivities: WorkoutActivity[] | undefined;
 
   constructor(private workout: WorkoutService) { }
@@ -19,10 +26,23 @@ export class WorkoutActivityComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.subs.sink = this.workout.getWorkoutActivity(1, 5)
-      .subscribe(activityData => {
-        this.workoutActivities = activityData.data;
+    this.subs.sink = this.pageNumber$
+      .pipe(
+        switchMap(page => this.workout.getWorkoutActivity(page, this.perPage)),
+        tap(page => this.noMoreRecords = page.totalRecords <= this.pageNumber$.getValue() * this.perPage),
+        map(page => page.data),
+        scan((acc, value) => [...acc, ...value])
+      )
+      .subscribe(data => {
+        this.workoutActivities = data;
+        this.loadingMore = false;
       });
+  }
+
+  loadMore() {
+    this.loadingMore = true;
+    const currentPage = this.pageNumber$.getValue();
+    this.pageNumber$.next(currentPage + 1);
   }
 
   private subs = new SubSink();
