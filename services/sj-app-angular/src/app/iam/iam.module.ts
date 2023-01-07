cf8d40ad059defe4f8faba2fd4ee598b2b2146dc
Joel Johnston <colter13@gmail.com>
2023-01-07 15:15:02 -0700

    Stub in login page

--- services/sj-app-angular/src/app/iam/iam.module.ts
+++ services/sj-app-angular/src/app/iam/iam.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class IamModule { }
