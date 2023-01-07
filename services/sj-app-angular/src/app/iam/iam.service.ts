ae0bf3ff62af99236c2d5c8b00c4c6c17367b643
Joel Johnston <colter13@gmail.com>
2023-01-07 15:41:08 -0700

    Work on login screen

--- services/sj-app-angular/src/app/iam/iam.service.ts
+++ services/sj-app-angular/src/app/iam/iam.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IamService {
 
-  constructor() { }
+  private readonly BASE_URL = '/services/iam'
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.BASE_URL}/account/login`, { username, password });
+  }
+
 }
--- services/sj-app-angular/src/app/iam/login/login.component.ts
+++ services/sj-app-angular/src/app/iam/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+import { ToastService } from 'src/app/services/toast.service';
+import { IamService } from '../iam.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formEmail: string = '';
+  formPassword: string = '';
+
+  processing: boolean = false;
+
+  constructor(
+    private iam: IamService,
+    private toast: ToastService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.processing = true;
+    this.iam.login(this.formEmail, this.formPassword)
+      .subscribe({
+        next: (data) => {
+          const token = data.token;
+          localStorage.setItem('app_token', token);
+          this.processing = false;
+          this.toast.setToast({ message: 'Login succeeded', domClass: 'bg-success text-light' });
+          this.router.navigate(['/']);
+        },
+        error: () => {
+          this.processing = false;
+          this.toast.setToast({ message: 'Login failed. Please confirm your username and password.', domClass: 'bg-warning text-light' });
+        } 
+      })
   }
 
 }
