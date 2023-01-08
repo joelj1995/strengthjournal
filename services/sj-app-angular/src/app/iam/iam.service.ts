8433b4c2232ca2f3002638114dd235a23279e98a
Joel Johnston <colter13@gmail.com>
2023-01-08 16:11:31 -0700

    Make login form more robust

--- services/sj-app-angular/src/app/iam/iam.service.ts
+++ services/sj-app-angular/src/app/iam/iam.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,37 @@ export class IamService {
 
   constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(`${this.BASE_URL}/account/login`, { username, password });
+  login(username: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.BASE_URL}/account/login`, { username, password })
+      .pipe(
+        catchError(err => {
+          try {
+            return of(err.error as LoginResponse);
+          } catch {
+            return of({
+              token: null,
+              result: LoginStatusCode.Unknown
+            });
+          }
+        })
+      );
   }
 
+  sendVerification(email: string): Observable<any> {
+    return this.http.post(`${this.BASE_URL}/account/send-verification`, { username: email });
+  }
+
+}
+
+export interface LoginResponse {
+  token: string | null;
+  result: LoginStatusCode
+}
+
+export enum LoginStatusCode {
+  Success = "Success",
+  WrongPassword = "WrongPassword",
+  ServiceFailure = "ServiceFailure",
+  EmailNotVerified = "EmailNotVerified",
+  Unknown = "Unknown"
 }
--- services/sj-app-angular/src/app/iam/login/login.component.ts
+++ services/sj-app-angular/src/app/iam/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs';
 import { ToastService } from 'src/app/services/toast.service';
-import { IamService } from '../iam.service';
+import { IamService, LoginResponse, LoginStatusCode } from '../iam.service';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +11,16 @@ import { IamService } from '../iam.service';
 })
 export class LoginComponent implements OnInit {
 
+  readonly loginErrorMessages = {
+    'WrongPassword': 'The username and password combination you used is not correct.',
+    'ServiceFailure': 'There was an error processing your login. Please try again in a few minutes.',
+    'Unknown': 'There was an error processing your loging. Please try again in a few minutes.'
+  }
+
   formEmail: string = '';
   formPassword: string = '';
+
+  unverifiedEmail: string | null = null;
 
   processing: boolean = false;
 
@@ -26,21 +34,44 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
+    this.unverifiedEmail = null;
     this.processing = true;
     this.iam.login(this.formEmail, this.formPassword)
+      .subscribe(data => this.processLoginResponse(data));
+  }
+
+  processLoginResponse(data: LoginResponse) {
+    this.processing = false;
+    switch (data.result) {
+      case LoginStatusCode.Success: {
+        if (!data.token) throw 'Got a null token for a successful login result';
+        const token = data.token;
+        localStorage.setItem('app_token', token);
+        this.processing = false;
+        this.toast.setToast({ message: 'Login succeeded', domClass: 'bg-success text-light' });
+        this.router.navigate(['/']);
+        break;
+      }
+      case LoginStatusCode.WrongPassword:
+      case LoginStatusCode.ServiceFailure:
+      case LoginStatusCode.Unknown: {
+        this.toast.setToast({ message: this.loginErrorMessages[data.result], domClass: 'bg-danger text-light' });
+        break;
+      }
+      case LoginStatusCode.EmailNotVerified: {
+        this.unverifiedEmail = this.formEmail;
+      }
+    }
+  }
+
+  resendVerification() {
+    if (!this.unverifiedEmail) return;
+    this.toast.setToast({ message: 'Sending verification email...', domClass: 'bg-info text-light' })
+    this.iam.sendVerification(this.unverifiedEmail)
       .subscribe({
-        next: (data) => {
-          const token = data.token;
-          localStorage.setItem('app_token', token);
-          this.processing = false;
-          this.toast.setToast({ message: 'Login succeeded', domClass: 'bg-success text-light' });
-          this.router.navigate(['/']);
-        },
-        error: () => {
-          this.processing = false;
-          this.toast.setToast({ message: 'Login failed. Please confirm your username and password.', domClass: 'bg-warning text-light' });
-        } 
-      })
+        next: () => this.toast.setToast({ message: 'Verification sent!', domClass: 'bg-success text-light' }),
+        error: () => this.toast.setToast({ message: 'An error occured re-sending the verification email. Please try again later.', domClass: 'bg-danger text-light' })
+      });
   }
 
 }
