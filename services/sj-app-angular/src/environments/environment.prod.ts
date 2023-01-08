bb83a0d5d15d92d4fccf94006738be6b8ef06c65
Joel Johnston <colter13@gmail.com>
2023-01-08 12:42:00 -0700

    Fix some URLs

--- services/sj-app-angular/src/environments/environment.prod.ts
+++ services/sj-app-angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   useResourceOwnerFlow: true,
-  api: '/services/api',
+  api: '/api/journal',
   version: '{REVISION_HASH}',
   features: ['Stub']
 };
