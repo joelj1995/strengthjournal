40191e7dee32ae4ec468d06696d3f1e46cc71f18
Joel Johnston <colter13@gmail.com>
2023-01-08 21:32:14 -0700

    Parameterize nginx config

---
+++ services/push-images.ps1
@@ -0,0 +1,11 @@
+docker tag services-gateway strengthjournalservices.azurecr.io/samples/services-gateway
+docker push strengthjournalservices.azurecr.io/samples/services-gateway
+
+docker tag services-mvc strengthjournalservices.azurecr.io/samples/services-mvc
+docker push strengthjournalservices.azurecr.io/samples/services-mvc
+
+docker tag services-iam strengthjournalservices.azurecr.io/samples/services-iam
+docker push strengthjournalservices.azurecr.io/samples/services-iam
+
+docker tag services-journal strengthjournalservices.azurecr.io/samples/services-journal
+docker push strengthjournalservices.azurecr.io/samples/services-journal
