459258c4d0001dcd97edd384b314776f40410cba
Joel Johnston <colter13@gmail.com>
2023-01-09 18:10:40 -0700

    Get nginx config to play nice with container apps

--- services/push-images.ps1
+++ services/push-images.ps1
@@ -1,11 +1,11 @@
-docker tag services-gateway strengthjournalservices.azurecr.io/samples/services-gateway
-docker push strengthjournalservices.azurecr.io/samples/services-gateway
+docker tag services-gateway strengthjournalservices.azurecr.io/poc/services-gateway
+docker push strengthjournalservices.azurecr.io/poc/services-gateway
 
-docker tag services-mvc strengthjournalservices.azurecr.io/samples/services-mvc
-docker push strengthjournalservices.azurecr.io/samples/services-mvc
+docker tag services-mvc strengthjournalservices.azurecr.io/poc/services-mvc
+docker push strengthjournalservices.azurecr.io/poc/services-mvc
 
-docker tag services-iam strengthjournalservices.azurecr.io/samples/services-iam
-docker push strengthjournalservices.azurecr.io/samples/services-iam
+docker tag services-iam strengthjournalservices.azurecr.io/poc/services-iam
+docker push strengthjournalservices.azurecr.io/poc/services-iam
 
-docker tag services-journal strengthjournalservices.azurecr.io/samples/services-journal
-docker push strengthjournalservices.azurecr.io/samples/services-journal
+docker tag services-journal strengthjournalservices.azurecr.io/poc/services-journal
+docker push strengthjournalservices.azurecr.io/poc/services-journal
