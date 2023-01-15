fc8d56f5eb3480bef7da6222146e9c104d546470
Joel Johnston <colter13@gmail.com>
2023-01-15 08:07:28 -0700

    Add health controller to MVC

---
+++ services/StrengthJournal.MVC/StrengthJournal.MVC/ApiControllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StrengthJournal.MVC.ApiControllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [Route("ping")]
+        public ActionResult Ping()
+        {
+            return Ok("mvc");
+        }
+
+        [Route("environment")]
+        public ActionResult GetEnvironment()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("AZ_ENVIRONMENT");
+            return Ok(environmentName ?? "N/A");
+        }
+    }
+}
