aad39feb6b1d8cb02509e764aa224176eedc913d
Joel Johnston <joel@joelj.ca>
2023-02-26 14:29:17 -0700

    Disable automated deployments

--- docker-compose-build.sh
+++ docker-compose-build.sh
@@ -1,5 +1,8 @@
 ENV_NAME=$1
 GIT_SHA=$2
 HOST_PORT=$3
+TARGET_HOST=$4
 
 ENV_NAME=$ENV_NAME GIT_SHA=$GIT_SHA HOST_PORT=$HOST_PORT envsubst < ./services/docker-compose.yml.template > docker-compose.yml
+
+scp docker-compose.yml service@$TARGET_HOST:/srv/strengthjournal/$ENV_NAME/
